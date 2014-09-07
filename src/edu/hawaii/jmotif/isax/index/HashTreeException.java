0d642c8957559dcde9e9b94d3bd3a38903f86d58
seninp <seninp@gmail.com>
2014-09-07 15:51:27 +0200

    migrating iSAX

---
+++ src/edu/hawaii/jmotif/isax/index/HashTreeException.java
@@ -0,0 +1,14 @@
+package edu.hawaii.jmotif.isax.index;
+
+public class HashTreeException extends Exception {
+
+  /**
+   *
+   */
+  private static final long serialVersionUID = 1L;
+
+  public HashTreeException(String string) {
+    // TODO Auto-generated constructor stub
+  }
+
+}
---
+++ src/edu/hawaii/jmotif/isax/index/SearchResults.java
@@ -0,0 +1,9 @@
+package edu.hawaii.jmotif.isax.index;
+
+import java.util.ArrayList;
+
+public class SearchResults {
+
+  ArrayList<TimeseriesInstance> results = new ArrayList<TimeseriesInstance>();
+
+}

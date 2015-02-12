68cd6e96e637c5f394277a6bb3af5afb6495d30b
Pavel Senin <seninp@gmail.com>
2015-02-12 13:50:41 +0100

    logical operator fix

--- src/edu/hawaii/jmotif/timeseries/TSUtils.java
+++ src/edu/hawaii/jmotif/timeseries/TSUtils.java
@@ -106,7 +106,7 @@ public final class TSUtils {
       }
       preRes.add(Double.valueOf(split[columnIdx]));
       lineCounter++;
-      if ((0 != sizeLimit && -1 != sizeLimit) && (lineCounter >= sizeLimit)) {
+      if ((0 != sizeLimit || -1 != sizeLimit) && (lineCounter >= sizeLimit)) {
         break;
       }
     }

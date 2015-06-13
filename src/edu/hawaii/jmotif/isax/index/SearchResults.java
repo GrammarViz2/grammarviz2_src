d71163c5ef3166b9aac3f4b118be1464721fe582
seninp <seninp@gmail.com>
2015-06-13 19:52:24 +0200

    La Gran Massacre

--- src/edu/hawaii/jmotif/isax/index/SearchResults.java
+++
@@ -1,9 +0,0 @@
-package edu.hawaii.jmotif.isax.index;
-
-import java.util.ArrayList;
-
-public class SearchResults {
-
-  ArrayList<TimeseriesInstance> results = new ArrayList<TimeseriesInstance>();
-
-}

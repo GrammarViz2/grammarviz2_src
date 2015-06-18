79845d21bc53e084b07a010740c35397e393adec
Pavel Senin <seninp@gmail.com>
2015-06-18 11:07:14 +0200

    prepearing a snapshot release

--- src/main/java/net/seninp/grammarviz/cli/TS2SequiturGrammar.java
+++ src/main/java/net/seninp/grammarviz/cli/TS2SequiturGrammar.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Date;
 import net.seninp.gi.GrammarRuleRecord;
 import net.seninp.gi.GrammarRules;
 import net.seninp.gi.RuleInterval;
@@ -97,9 +96,8 @@ public class TS2SequiturGrammar {
     // collect stats
     //
     consoleLogger.info("Producing the output ...");
-     String currentPath = new File(".").getCanonicalPath();
-     String fname = TS2GrammarParameters.OUT_FILE;
-    
+    String fname = TS2GrammarParameters.OUT_FILE;
+
     boolean fileOpen = false;
     BufferedWriter bw = null;
     try {
@@ -116,8 +114,7 @@ public class TS2SequiturGrammar {
       System.err.print("Encountered an error while writing stats file: \n" + StackTrace.toString(e)
           + "\n");
     }
-    
-    
+
     for (GrammarRuleRecord ruleRecord : rules) {
 
       StringBuffer sb = new StringBuffer();
@@ -156,7 +153,7 @@ public class TS2SequiturGrammar {
         }
       }
     }
-    
+
     // try to write stats into the file
     if (fileOpen) {
       try {
@@ -167,230 +164,6 @@ public class TS2SequiturGrammar {
             + StackTrace.toString(e) + "\n");
       }
     }
-    
-    
-    // SAXRecords saxFrequencyData = new SAXRecords();
-    //
-    // SAXRule.numRules = new AtomicInteger(0);
-    // SAXSymbol.theDigrams.clear();
-    // SAXSymbol.theSubstituteTable.clear();
-    // SAXRule.arrRuleRecords = new ArrayList<GrammarRuleRecord>();
-    //
-    // SAXRule grammar = new SAXRule();
-    //   // String currentPath = new File(".").getCanonicalPath();
-     // String fname = currentPath + File.separator + outputPrefix + "_sequitur_grammar_stat.txt";
-     //
-     // boolean fileOpen = false;
-     // BufferedWriter bw = null;
-     // try {
-     // bw = new BufferedWriter(new FileWriter(new File(fname)));
-     // StringBuffer sb = new StringBuffer();
-     // sb.append("# filename: ").append(fname).append(CR);
-     // sb.append("# sliding window: ").append(saxWindowSize).append(CR);
-     // sb.append("# paa size: ").append(saxPaaSize).append(CR);
-     // sb.append("# alphabet size: ").append(saxAlphabetSize).append(CR);
-     // bw.write(sb.toString());
-     // fileOpen = true;
-     // }
-     // catch (IOException e) {
-     // System.err.print("Encountered an error while writing stats file: \n" + StackTrace.toString(e)
-     // + "\n");
-     // }
-    // Date fullStart = new Date();
-    //
-    // try {
-    //
-    // String previousString = "";
-    //
-    // // scan across the time series extract sub sequences, and convert
-    // // them to strings
-    // int stringPosCounter = 0;
-    // for (int i = 0; i < originalTimeSeries.length - (saxWindowSize - 1); i++) {
-    //
-    // // fix the current subsection
-    // double[] subSection = Arrays.copyOfRange(originalTimeSeries, i, i + saxWindowSize);
-    //
-    // // Z normalize it
-    // if (tp.stDev(subSection) > NORMALIZATION_THRESHOLD) {
-    // subSection = tp.znorm(subSection, NORMALIZATION_THRESHOLD);
-    // }
-    //
-    // // perform PAA conversion if needed
-    // double[] paa = tp.paa(subSection, saxPaaSize);
-    //
-    // // Convert the PAA to a string.
-    // char[] currentString = tp.ts2String(paa, normalA.getCuts(saxAlphabetSize));
-    //
-    // // NumerosityReduction
-    // if (!previousString.isEmpty()) {
-    //
-    // if ((NumerosityReductionStrategy.MINDIST == numerosityReductionStrategy)
-    // && (0.0 == sp.saxMinDist(previousString.toCharArray(), currentString,
-    // normalA.getDistanceMatrix(saxAlphabetSize)))) {
-    // continue;
-    // }
-    // else if ((NumerosityReductionStrategy.EXACT == numerosityReductionStrategy)
-    // && previousString.equalsIgnoreCase(new String(currentString))) {
-    // continue;
-    // }
-    // }
-    //
-    // previousString = new String(currentString);
-    //
-    // grammar.last().insertAfter(new SAXTerminal(new String(currentString), stringPosCounter));
-    // grammar.last().p.check();
-    //
-    // saxFrequencyData.add(currentString, i);
-    //
-    // stringPosCounter++;
-    //
-    // }
-    //
-    // // String res = saxFrequencyData.getSAXString(" ");
-    // // res = null;
-    //
-    // Date end = new Date();
-    //
-    // System.out.println("Discretized and inferred grammar with Sequitur in  "
-    // + SAXProcessor.timeToString(fullStart.getTime(), end.getTime()));
-    //
-    // Date start = new Date();
-    // GrammarRules rules = grammar.toGrammarRulesData();
-    //
-    // SequiturFactory.updateRuleIntervals(rules, saxFrequencyData, true, originalTimeSeries,
-    // saxWindowSize, saxPaaSize);
-    // end = new Date();
-    //
-    // System.out.println("Expanded rules and computed intervals  in  "
-    // + String.valueOf(end.getTime() - start.getTime()) + " ms, "
-    // + SAXProcessor.timeToString(start.getTime(), end.getTime()));
-    //
-    // start = new Date();
-    //
-    // int[] coverageArray = new int[originalTimeSeries.length];
-    //
-    // for (GrammarRuleRecord r : rules) {
-    // if (0 == r.ruleNumber()) {
-    // continue;
-    // }
-    // ArrayList<RuleInterval> arrPos = r.getRuleIntervals();
-    // for (RuleInterval saxPos : arrPos) {
-    // int startPos = saxPos.getStartPos();
-    // int endPos = saxPos.getEndPos();
-    // for (int j = startPos; j < endPos; j++) {
-    // coverageArray[j] = coverageArray[j] + 1;
-    // }
-    // }
-    // }
-    // end = new Date();
-    // System.out.println("Computed rule coverage in  "
-    // + String.valueOf(end.getTime() - start.getTime()) + " ms, "
-    // + SAXProcessor.timeToString(start.getTime(), end.getTime()));
-    //
-    // System.out.println("Total runtime  " + String.valueOf(end.getTime() - fullStart.getTime())
-    // + " ms, " + SAXProcessor.timeToString(fullStart.getTime(), end.getTime()));
-    //
-    // // write down the coverage array
-    // //
-    // String currentPath = new File(".").getCanonicalPath();
-    // BufferedWriter bw = new BufferedWriter(new FileWriter(new File(currentPath + File.separator
-    // + outputPrefix + "_SEQUITUR_density_curve.txt")));
-    // for (int c : coverageArray) {
-    // bw.write(String.valueOf(c) + "\n");
-    // }
-    // bw.close();
-    //
-    // }
-    // catch (Exception ex) {
-    // ex.printStackTrace();
-    // }
-    //
-    // }
-    //
-    // @SuppressWarnings("unused")
-    // private static String collectMotifStats(SAXRule grammar) throws IOException {
-    //
-    // // start collecting stats
-    // //
-    // consoleLogger.info("Collecting stats:");
-    // String currentPath = new File(".").getCanonicalPath();
-    // String fname = currentPath + File.separator + outputPrefix + "_sequitur_grammar_stat.txt";
-    //
-    // boolean fileOpen = false;
-    // BufferedWriter bw = null;
-    // try {
-    // bw = new BufferedWriter(new FileWriter(new File(fname)));
-    // StringBuffer sb = new StringBuffer();
-    // sb.append("# filename: ").append(fname).append(CR);
-    // sb.append("# sliding window: ").append(saxWindowSize).append(CR);
-    // sb.append("# paa size: ").append(saxPaaSize).append(CR);
-    // sb.append("# alphabet size: ").append(saxAlphabetSize).append(CR);
-    // bw.write(sb.toString());
-    // fileOpen = true;
-    // }
-    // catch (IOException e) {
-    // System.err.print("Encountered an error while writing stats file: \n" + StackTrace.toString(e)
-    // + "\n");
-    // }
-    //
-    // GrammarRules rules = grammar.toGrammarRulesData();
-    //
-    // // SequiturFactory.updateRuleIntervals(rules, saxFrequencyData, true, originalTimeSeries,
-    // // saxWindowSize, saxPaaSize);
-    //
-    // for (GrammarRuleRecord ruleRecord : rules) {
-    //
-    // StringBuffer sb = new StringBuffer();
-    // sb.append("/// ").append(ruleRecord.getRuleName()).append(CR);
-    // sb.append(ruleRecord.getRuleName()).append(" -> \'")
-    // .append(ruleRecord.getRuleString().trim()).append("\', expanded rule string: \'")
-    // .append(ruleRecord.getExpandedRuleString()).append("\'").append(CR);
-    //
-    // if (!ruleRecord.getOccurrences().isEmpty()) {
-    // sb.append("subsequences starts: ")
-    // .append(
-    // Arrays.toString(ruleRecord.getOccurrences().toArray(
-    // new Integer[ruleRecord.getOccurrences().size()]))).append(CR);
-    // int[] lengths = new int[ruleRecord.getRuleIntervals().size()];
-    // int i = 0;
-    // for (RuleInterval r : ruleRecord.getRuleIntervals()) {
-    // lengths[i] = r.getEndPos() - r.getStartPos();
-    // i++;
-    // }
-    // sb.append("subsequences lengths: ").append(Arrays.toString(lengths)).append(CR);
-    // }
-    //
-    // sb.append("rule occurrence frequency ").append(ruleRecord.getOccurrences().size()).append(CR);
-    // sb.append("rule use frequency ").append(ruleRecord.getRuleUseFrequency()).append(CR);
-    // sb.append("min length ").append(ruleRecord.minMaxLengthAsString().split(" - ")[0]).append(CR);
-    // sb.append("max length ").append(ruleRecord.minMaxLengthAsString().split(" - ")[1]).append(CR);
-    // sb.append("mean length ").append(ruleRecord.getMeanLength()).append(CR);
-    //
-    // if (fileOpen) {
-    // try {
-    // bw.write(sb.toString());
-    // }
-    // catch (IOException e) {
-    // System.err.print("Encountered an error while writing stats file: \n"
-    // + StackTrace.toString(e) + "\n");
-    // }
-    // }
-    // }
-    //
-    // // try to write stats into the file
-    // if (fileOpen) {
-    // try {
-    // bw.close();
-    // }
-    // catch (IOException e) {
-    // System.err.print("Encountered an error while writing stats file: \n"
-    // + StackTrace.toString(e) + "\n");
-    // }
-    // }
-    //
-    // return null;
-    // }
-    //
- 
+
   }
 }

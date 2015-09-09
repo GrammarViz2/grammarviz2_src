474a91a878fc85c22e803e409790918d562320e1
seninp <seninp@gmail.com>
2015-09-09 10:50:35 +0200

    updating the figure

--- RCode/paperworks/video_figure_1.R
+++ RCode/paperworks/video_figure_1.R
@@ -37,29 +37,58 @@ require(gtable)
 #
 data=read.csv(file="paperworks/runlogs/anomaly_01.csv",header=F,sep=",")
 df=data.frame(time=c(1:length(data$V1)),value=data$V1)
-p <- ggplot(df, aes(time, value)) + geom_line(lwd=1.1,color="blue1") + theme_classic() +
+p0 <- ggplot(df, aes(time, value)) + geom_line(lwd=0.6,color="grey10") + theme_classic() +
   ggtitle("Excerpt from the Video dataset") + 
+  theme(plot.title = element_text(size = rel(1.8)), axis.title.x = element_blank(),axis.title.y=element_blank(),
+        axis.ticks.y=element_blank(),axis.text.y=element_blank(),axis.text.x=element_text(size = rel(1.8)))
+p0  
+p <- ggplot(df, aes(time, value)) + geom_line(lwd=0.6,color="grey10") + theme_classic() +
+  ggtitle("Motifs and discords discovered in Video dataset") + 
   theme(plot.title = element_text(size = rel(1.8)), axis.title.x = element_blank(),axis.title.y=element_blank(),
         axis.ticks.y=element_blank(),axis.text.y=element_blank(),axis.text.x=element_text(size = rel(1.8)))
 p  
 red_line=df[1099:(1099+42),]
 p = p + geom_line(data=red_line,col="red", lwd=1.6)
 green_line=df[1313:(1313+107),]
-p = p + geom_line(data=green_line,col="green", lwd=1.6)
+p = p + geom_line(data=green_line,col="red", lwd=1.6)
 violet_line=df[1599:(1599+181),]
-p = p + geom_line(data=violet_line,col="deeppink", lwd=1.6)
+p = p + geom_line(data=violet_line,col="red", lwd=1.6)
+
+motifs <- 
+  "65,242
+224,400
+369,545
+525,702
+665,841
+820,996
+1569,1744
+1869,2044
+2029,2205
+2177,2353
+2320,2496
+2465,2641
+2614,2789
+2768,2944
+2918,3094
+3069,3245
+3226,3401"
+dat = read.delim(textConnection(motifs),
+                 header=FALSE,sep=",",strip.white=TRUE)
+for(i in 1:length(dat$V1)){
+  p = p + geom_line(data=df[dat[i,1]:dat[i,2],],col="blue", lwd=1.6)
+}
 p
+
 #
 density=read.csv(file="paperworks/runlogs/rra_out_coverage.txt",header=F,sep=",")
 which(density==0)
 density_df=data.frame(time=c(1:length(density$V1)),value=density$V1)
-density_df$value[400:600]=density_df$value[400:600]-1
-shade <- rbind(c(0,0), density_df, c(2229,0))
+shade <- rbind(c(0,0), density_df, c(3502,0))
 names(shade)<-c("x","y")
 p1 <- ggplot(density_df, aes(x=time,y=value)) +
   geom_line(col="cyan2") + theme_classic() + scale_y_continuous(limits = c(0, 36)) +
   geom_polygon(data = shade, aes(x, y), fill="cyan", alpha=0.5) +
-  ggtitle("Grammar rules density") + 
+  ggtitle("Grammar rules density (132 rules)") + 
   theme(plot.title = element_text(size = rel(1.8)), axis.title.x = element_blank(),axis.title.y=element_blank(),
         axis.text.y=element_text(size = rel(1.8)),axis.text.x=element_text(size = rel(1.8)))
 p1  
@@ -67,13 +96,12 @@ p1
 density2=read.csv(file="paperworks/runlogs/rrapruned_out_coverage.txt",header=F,sep=",")
 which(density2==0)
 density_df=data.frame(time=c(1:length(density2$V1)),value=density2$V1)
-density_df$value[400:600]=density_df$value[400:600]-1
-shade <- rbind(c(0,0), density_df, c(2229,0))
+shade <- rbind(c(0,0), density_df, c(3502,0))
 names(shade)<-c("x","y")
 p2 <- ggplot(density_df, aes(x=time,y=value)) +
   geom_line(col="cyan2") + theme_classic() + scale_y_continuous(limits = c(0, 8), breaks=c(0,2,4,6)) +
   geom_polygon(data = shade, aes(x, y), fill="cyan", alpha=0.5) +
-  ggtitle("Pruned grammar rules density") + 
+  ggtitle("Pruned grammar rules density (12 rules)") + 
   theme(plot.title = element_text(size = rel(1.8)), axis.title.x = element_blank(),axis.title.y=element_blank(),
         axis.text.y=element_text(size = rel(1.8)),axis.text.x=element_text(size = rel(1.8)))
 p2  
@@ -125,11 +153,11 @@ cbind_gtable_max <- function(...){
   Reduce(bind2, gtl)
 }
 
-grid.draw(rbind_gtable_max(ggplotGrob(p), ggplotGrob(p1), ggplotGrob(p2)))
+grid.draw(rbind_gtable_max(ggplotGrob(p0), ggplotGrob(p1), ggplotGrob(p2), ggplotGrob(p)))
 
-Cairo(width = 1000, height = 700, 
+Cairo(width = 1000, height = 850, 
       file="video_excerpt.pdf", 
       type="pdf", pointsize=12, 
       bg = "transparent", canvas = "white", units = "px", dpi = 82)
-print(grid.draw(rbind_gtable_max(ggplotGrob(p), ggplotGrob(p1), ggplotGrob(p2))))
+print(grid.draw(rbind_gtable_max(ggplotGrob(p0), ggplotGrob(p1), ggplotGrob(p2), ggplotGrob(p))))
 dev.off()

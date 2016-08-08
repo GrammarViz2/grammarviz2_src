c4c3ccd638421596e5788f5c20ecea9bf1839dff
Pavel Senin <seninp@gmail.com>
2016-08-08 09:43:47 -0600

    paperworks: laptop sync

--- RCode/sine_tinker.R
+++ RCode/sine_tinker.R
@@ -27,30 +27,32 @@ points <- which(t >= anomaly_pos[3] & t <= anomaly_pos[3] + pi)
 y[points] <- -1
 p2 <- ggplot(data.frame(x = 1:length(y), y = y), aes(x, y)) + geom_line() + ggtitle("Sine wave + anomaly")
 p2
-write.table(y, "../data/aa_synth/sine_0_a.txt", col.names = F, row.names = F)
+write.table(y, "../data/aa_synth/sine_1_a.txt", col.names = F, row.names = F)
 #
 # adding an anomaly of stretch and compression
 #
 # compression
 #
-anomaly_pos2 = c( runif(1) * (max(t) - pi *6), runif(1) * (max(t) - pi *6) )
+k = 376.8/(length(t))
+#anomaly_pos2 = c( runif(1) * (max(t) - pi *6), runif(1) * (max(t) - pi *6) )
+anomaly_pos2 <- c( 360*k, 508*k)
 points2 <- which(t >= anomaly_pos2[1] & t <= anomaly_pos2[1] + pi * 6)
 points2 <- points2[ seq(1, length(points2), by = 2) ]
 y <- y[ -points2 ]
 p3 <- ggplot(data.frame(x = 1:length(y), y = y), aes(x, y)) + geom_line() + ggtitle("Sine wave + anomaly")
 p3
-write.table(y, "../data/aa_synth/sine_0_a1.txt", col.names = F, row.names = F)
+write.table(y, "../data/aa_synth/sine_1_a1.txt", col.names = F, row.names = F)
 #
 # stretch
 # 
 points3 <- which(t >= anomaly_pos2[2] & t <= anomaly_pos2[2] + pi * 1.5)
-points3_new <- sin( seq(min(points3), max(points3), by = 0.15) )
-y <- c( y[1:(min(points3))], points3_new, y[(max(points3)):length(y)])
-p4 <- ggplot(data.frame(x = 1:length(y), y = y), aes(x, y)) + geom_line() + ggtitle("Sine wave + anomaly")
+points3_new <- sin( seq(t[min(points3)], t[max(points3)], by = 0.15) - length(points2) * 0.3)
+y1 <- y
+y[points3]
+y1 <- c( y[1:(min(points3))], points3_new, y[(max(points3)):length(y)])
+p4 <- ggplot(data.frame(x = 1:length(y1), y = y1), aes(x, y)) + geom_line() + ggtitle("Sine wave + anomaly")
 p4
-write.table(y, "../data/aa_synth/sine_0_a1.txt", col.names = F, row.names = F)
-
-
+write.table(y1, "../data/aa_synth/sine_0_a2.txt", col.names = F, row.names = F)
 
 p3 <- ggplot(data.frame(x = t, y = y), aes(x, y)) + geom_line() + ggtitle("Sine wave + anomaly")
 p3
@@ -61,3 +63,12 @@ p4 <- p3 + geom_line(aes(y=df_red$y,x=df_red$x), color = "brown3", size = 1)
 p4
 
 write.table(y, "../data/aa_synth/sine_0_a2.txt", col.names = F, row.names = F)
+
+#
+#
+#
+library(data.table)
+dd = fread("../res.txt")
+density(dd$V3)
+ggplot(data=dd, aes(dd$V5)) + geom_density()
+plot(y[18:100])
